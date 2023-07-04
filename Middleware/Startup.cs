b5b85391a8fc12af1dbea5311e46d2d7943586b5
lutfihakim123 <lutfirahman400@gmail.com>
2023-07-04 11:31:01 -0800

    add razor runtime compilation

--- Middleware/Startup.cs
+++ Middleware/Startup.cs
@@ -27,7 +27,7 @@ namespace Middleware
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
