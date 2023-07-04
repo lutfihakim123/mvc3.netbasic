2dd7fe277fd4239b2b38ae50154bcbe3d29fc6d0
lutfihakim123 <lutfirahman400@gmail.com>
2023-07-04 09:32:14 -0800

    initialize project

---
+++ Middleware/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello";
+        }
+    }
+}
