abce67436c492f8e03e6f7f93d2b28c4fb4b8f9e
lutfihakim123 <lutfirahman400@gmail.com>
2023-07-04 09:49:11 -0800

    add book controller

---
+++ Middleware/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Controllers
+{
+    public class BookController : Controller
+    {
+        public string ShowBook()
+        {
+            return "All Books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id = {id}";
+        }
+        public string SearchBooks(string bookName, string authorName) {
+
+            return $"book with name = {bookName} & authorname = {authorName}";
+        }
+    }
+}
